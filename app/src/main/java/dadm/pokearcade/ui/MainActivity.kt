6a7a915f874f47c9d64fc75967b7d5cfab35819d
Devalent20 <99796127+Devalent20@users.noreply.github.com>
2023-05-11 19:31:19 +0200

    Action bar para visualizar el título y main activiy documentado

--- app/src/main/java/dadm/pokearcade/ui/MainActivity.kt
+++ app/src/main/java/dadm/pokearcade/ui/MainActivity.kt
@@ -6,12 +6,17 @@ import android.view.View
 import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import dadm.pokearcade.R
 import dadm.pokearcade.databinding.ActivityMainBinding
 
+/**
+ * Class main activity
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +29,38 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView as NavigationBarView
         binding.bottomNavigationView.setupWithNavController(navController)
 
+        /**
+         * The navigation bar only show on the game, profile and settings fragment.
+         */
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.loginFragment -> {
-                    binding.bottomNavigationView.visibility = View.INVISIBLE
-                }
-                R.id.signupFragment -> {
-                    binding.bottomNavigationView.visibility = View.INVISIBLE
-                }
-                else -> {
+                R.id.gamesFragment -> {
                     binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.profileFragment -> {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+                R.id.settingsFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                /** If not, invisible */
+                else -> {
+                    binding.bottomNavigationView.visibility = View.INVISIBLE
                 }
             }
         }
+
+        /**
+         * The configuration of the action bar
+         */
+        setSupportActionBar(binding.idMaterialToolBar)
+        var appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.gamesFragment,
+                R.id.profileFragment,
+                R.id.settingsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 }
