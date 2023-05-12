b68be541d0351e58c9d0e2d0f37ee87f1800b897
ZhuGH333 <yzhupingn@gmail.com>
2023-05-12 17:52:00 +0200

    Settings UI (solo la interfaz)

--- app/src/main/java/dadm/pokearcade/ui/MainActivity.kt
+++ app/src/main/java/dadm/pokearcade/ui/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         /**
          * The navigation bar only show on the game, profile and settings fragment.
          */
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.gamesFragment -> {
