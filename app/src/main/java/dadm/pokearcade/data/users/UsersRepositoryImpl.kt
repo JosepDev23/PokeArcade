5e154d5845302483073f7c6076182ebb244771a1
Devalent20 <99796127+Devalent20@users.noreply.github.com>
2023-05-13 18:01:14 +0200

    Documentadas clases y traducción funcional.

--- app/src/main/java/dadm/pokearcade/data/users/UsersRepositoryImpl.kt
+++ app/src/main/java/dadm/pokearcade/data/users/UsersRepositoryImpl.kt
@@ -7,20 +7,26 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+/**
+ * Implement UserRepository with the CRUD operations.
+ */
 class UsersRepositoryImpl @Inject constructor(
     private val usersDataSource: UsersDataSource
 ): UsersRepository {
 
+    /** Register a new user with the user domain. */
     override suspend fun postUser(user: User) {
         usersDataSource.postUser(user.toDto())
     }
 
+    /** Get the user with the username and password. */
     override fun getUserByUsernameAndPassword(username: String, password: String): Flow<User?> {
         return usersDataSource.getUserByUsernameAndPassword(username, password).map {
             it?.toDomain()
         }
     }
 
+    /** update the userDTO with new info. */
     override suspend fun updateUser(user: User): Int {
         return usersDataSource.updateUser(user.toDto())
     }
--- app/src/main/java/dadm/pokearcade/data/users/model/UserDtoMapper.kt
+++ app/src/main/java/dadm/pokearcade/data/users/model/UserDtoMapper.kt
@@ -2,6 +2,9 @@ package dadm.pokearcade.data.users.model
 
 import dadm.pokearcade.domain.model.User
 
+/**
+ * Mapping from user DTO to Domain.
+ */
 fun UserDto.toDomain(): User = User(
     username = username,
     password = password,
@@ -9,6 +12,9 @@ fun UserDto.toDomain(): User = User(
     games = games
 )
 
+/**
+ * Mapping from user domain to DTO.
+ */
 fun User.toDto(): UserDto = UserDto(
     username = username,
     password = password,
