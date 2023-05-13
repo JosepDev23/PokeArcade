da30d0ef38131e25f475ae82e69bfc73bb65c255
JosepDev23 <josepdev23@gmail.com>
2023-05-13 18:10:25 +0200

    profile fino

--- app/src/main/java/dadm/pokearcade/ui/profile/ProfileFragment.kt
+++ app/src/main/java/dadm/pokearcade/ui/profile/ProfileFragment.kt
@@ -1,22 +1,35 @@
 package dadm.pokearcade.ui.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import dadm.pokearcade.R
 import dadm.pokearcade.databinding.FragmentProfileBinding
+import dadm.pokearcade.ui.login.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Represents the current user information with the username, a default profile picture
  * and the number of games played and won.
  */
+@AndroidEntryPoint
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
+    private val loginViewModel: LoginViewModel by activityViewModels()
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentProfileBinding.bind(view)
+        val user = loginViewModel.user.value
+
+        binding.usernameProfile.text = getString(R.string.username_profile, user?.username)
+        binding.totalWins.text = getString(R.string.total_wins, user?.wins)
+        binding.totalGames.text = getString(R.string.total_games, user?.games)
     }
 
     override fun onDestroyView() {
