7395d9ee3f430889b8f7b62200419e9366383739
JosepDev23 <josepdev23@gmail.com>
2023-05-13 16:39:35 +0200

    login funcional

--- app/src/main/java/dadm/pokearcade/ui/login/LoginFragment.kt
+++ app/src/main/java/dadm/pokearcade/ui/login/LoginFragment.kt
@@ -5,17 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import dadm.pokearcade.R
 import dadm.pokearcade.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Represents the login functionality of the app. It contains methods for user authentication
  * and the navigation to user signup.
  */
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,9 +36,21 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.bind(view)
 
         binding.loginButton.setOnClickListener {
-            val action = LoginFragmentDirections.actionLoginFragmentToGamesFragment()
-            findNavController().navigate(action)
+            val username = binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            viewModel.loginUser(username, password)
         }
+
+        viewModel.loginStatus.observe(viewLifecycleOwner) { result ->
+            if (result.isSuccess) {
+                val action = LoginFragmentDirections.actionLoginFragmentToGamesFragment()
+                findNavController().navigate(action)
+            } else {
+                Snackbar.make(binding.root, "Wrong username or password", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
 
         binding.registerTextView.setOnClickListener {
             val action = LoginFragmentDirections.actionLoginFragmentToSignupFragment()
--- app/src/main/java/dadm/pokearcade/ui/login/LoginViewModel.kt
+++ app/src/main/java/dadm/pokearcade/ui/login/LoginViewModel.kt
@@ -3,8 +3,12 @@ package dadm.pokearcade.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dadm.pokearcade.data.users.UsersRepository
+import dadm.pokearcade.domain.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,15 +16,27 @@ class LoginViewModel @Inject constructor(
     var usersRepository: UsersRepository
 ) : ViewModel() {
 
+    private val _user = MutableLiveData<User?>()
+    val user: LiveData<User?> get() = _user
+
     private val _loginStatus = MutableLiveData<Result<Unit>>()
     val loginStatus: LiveData<Result<Unit>> get() = _loginStatus
 
     fun loginUser(username: String, password: String) {
-        try {
-            usersRepository.getUserByUsernameAndPassword(username, password)
-            _loginStatus.value = Result.success(Unit)
-        } catch (e: Exception) {
-            _loginStatus.value = Result.failure(e)
+        viewModelScope.launch {
+            try {
+                val userFlow = usersRepository.getUserByUsernameAndPassword(username, password)
+                userFlow.collect {
+                    if (it != null) {
+                        _loginStatus.value = Result.success(Unit)
+                        _user.value = it
+                    } else {
+                        _loginStatus.value = Result.failure(Exception("User not found!"))
+                    }
+                }
+            } catch (e: Exception) {
+                _loginStatus.value = Result.failure(e)
+            }
         }
     }
 }
